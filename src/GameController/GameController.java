c98c759c0619d4b3a277fb7aa806e826df34cbef
ponomarev-vitaly <vp211082@gmail.com>
2023-03-29 06:40:01 +0300

    I added gamePlay method. Application runs smoothly with basic functions.

--- src/GameController/GameController.java
+++ src/GameController/GameController.java
@@ -33,41 +33,84 @@ public class GameController {
         Selection   -> S
          */
 
-        do{
-            if(isMainMenu){
+        do {
+            if (isMainMenu) {
                 input = mainMenu();
-                if(input == 'D'){
-                    if(mainMenuOption == 1){
+                if (input == 'D') {
+                    if (mainMenuOption == 1) {
                         mainMenuOption = 2;
-                    } else if (input == 'U'){
-                        if(mainMenuOption == 2){
+                    }
+                } else if (input == 'U') {
+                        if (mainMenuOption == 2) {
                             mainMenuOption = 1;
                         }
-                    }  else if (input == 'S') {
-                        if(mainMenuOption == 1){
-                            System.out.println("User can select game.");
-                        }else{
-                            System.out.println("User can play the game.");
+                    } else if (input == 'S') {
+                        if (mainMenuOption == 1) {
+                            isGameSelect = true;
+                            isMainMenu = false;
+                            mainMenuOption = 1;
+                        } else {
+                            isGamePlay = true;
+                            isMainMenu = false;
+                            mainMenuOption = 1;
                         }
                     }
+            }
+
+            if (isGameSelect) {
+                input = gameMenu();
+                if (input == 'D') {
+                    if (gameMenuOption != 3) {
+                        gameMenuOption++;
+                    } else if (input == 'U') {
+                        if (gameMenuOption != 1) {
+                            gameMenuOption--;
+                        }
+                    }
+                } else if (input == 'S') {
+                    isMainMenu = true;
+                    isGameSelect = false;
+                    selectedGame = gameMenuOption - 1;
+                    gameMenuOption = 1;
                 }
             }
 
-            if(isGameSelect){
-                // TODO
-            }
-
-            if(isGamePlay){
-                // TODO
+            if (isGamePlay) {
+                input = gamePlay();
+                switch (input){
+                    case 'U':
+                        games.get(selectedGame).up();
+                        break;
+                    case 'D':
+                        games.get(selectedGame).down();
+                        break;
+                    case 'R':
+                        games.get(selectedGame).right();
+                        break;
+                    case 'L':
+                        games.get(selectedGame).left();
+                        break;
+                    case 'A':
+                        games.get(selectedGame).A();
+                        break;
+                    case 'B':
+                        games.get(selectedGame).B();
+                        break;
+                    case 'S':
+                        isGamePlay = false;
+                        isMainMenu = true;
+                        break;
+                }
             }
             System.out.println("\n\n");
-        }while(input != 'p');
+        }while (input != 'p');
     }
+
 
     // If we call method in static method the method have to be static as well.
     public static char mainMenu(){
         System.out.println("----------MAIN MENU----------");
-        System.out.println("-------SELECTED GAME: -------");
+        System.out.println("- SELECTED GAME: " + (selectedGame == 0 ? "SONIC": (selectedGame == 1 ? "MARIO" : "CONTRA")) + " -");
         if(mainMenuOption == 1){
             System.out.println("\t -> Game Select <- ");
             System.out.println("Game Start");
@@ -75,7 +118,36 @@ public class GameController {
             System.out.println("Game Select");
             System.out.println("\t -> Game Start <- ");
         }
-        System.out.println("---(D)own--(U)p--(S)elect-----");
+        System.out.print("---(D)own--(U)p--(S)elect-----");
+        return scan.next().charAt(0);
+    }
+
+    public static char gameMenu(){
+        System.out.println("----------GAME MENU----------");
+        System.out.println("- SELECTED GAME: " + (selectedGame == 0 ? "SONIC": (selectedGame == 1 ? "MARIO" : "CONTRA")) + " -");
+        if(gameMenuOption == 1){
+            System.out.println("\t -> SONIC <- ");
+            System.out.println("MARIO");
+            System.out.println("CONTRA");
+        } else if(gameMenuOption == 2) {
+            System.out.println("SONIC");
+            System.out.println("\t -> MARIO <- ");
+            System.out.println("CONTRA");
+        } else if(gameMenuOption == 3) {
+            System.out.println("SONIC");
+            System.out.println("MARIO");
+            System.out.println("\t -> CONTRA <- ");
+        }
+        return scan.next().charAt(0);
+    }
+
+    public static char gamePlay(){
+        System.out.println("----------GAME PLAY----------");
+        System.out.println("- SELECTED GAME: " + (selectedGame == 0 ? "SONIC": (selectedGame == 1 ? "MARIO" : "CONTRA")) + " -");
+        System.out.println("---(D)own--(U)p--Button(A)-----");
+        System.out.println("---(D)own--(U)p--Button(B)-----");
+        System.out.println("-----(S)witch to Main Menu-----");
+
         return scan.next().charAt(0);
     }
 }
